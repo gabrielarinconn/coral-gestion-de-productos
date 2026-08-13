19dd67976fb3d21c01d45f006c24d414e3dac631
andrealizcano <andrealizcano2023@gmail.com>
2026-08-13 12:39:25 -0500

    se realiza layout de navbar y footer

--- frontend/src/app/shared/components/layout/layout.ts
+++ frontend/src/app/shared/components/layout/layout.ts
@@ -1,11 +1,14 @@
     import { Component } from '@angular/core';
+    import {RouterLink} from '@angular/router';
+    import {NgIf} from '@angular/common'
 
     @Component({
     selector: 'app-layout',
+    imports: [RouterLink, NgIf],
     templateUrl: './layout.html',
     styleUrls: ['./layout.css']
     })
-    export class LayoutComponent {
+    export class Layout {
     isLoggedIn: boolean = false; // Este será un marcador de posición para el estado de inicio de sesión
 
     toggleLogin(): void {
